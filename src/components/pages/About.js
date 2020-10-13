a9db32f61b2bb83f25e21a032c3de0d814d4b4d7
Pijush Konar <59543014+pijush-konar-au7@users.noreply.github.com>
2020-10-13 13:03:13 +0530

    Final Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to search Github Users</p>
+      <p>Developed by <a href="https://github.com/pijush-konar-au7">Pijush</a></p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
